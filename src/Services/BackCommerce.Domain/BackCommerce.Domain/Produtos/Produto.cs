fea40aadd3e56dcfa45e14aef95786a67a887b66
Lucas Carvalho <dev8@automatizasistemas.com.br>
2023-05-26 21:29:27 -0300

    Criando camada de dominio e primeiro testes para o cadastro de produto utilizando TDD

---
+++ src/Services/BackCommerce.Domain/BackCommerce.Domain/Produtos/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace BackCommerce.Domain.Produtos;
+
+public class Produto
+{
+    public string Descricao { get;private set; }
+    public decimal Valor { get;private set; }
+    public string Imagem { get; private set; }
+    public string Categoria { get; private set; }
+    public int QuantidadeEmEstoque { get; private set; }
+    public Produto(string descricao, decimal valor, string imagem, string categoria, int quantidadeEmEstoque)
+    {
+        if (string.IsNullOrEmpty(descricao))
+            throw new Exception("O produto deve ter uma descrição");
+        if (valor <= 0)
+            throw new Exception("O valor do produto deve ser maior que zero");
+        if (string.IsNullOrEmpty(imagem))
+            throw new Exception("O produto deve ter uma imagem");
+        if (string.IsNullOrEmpty(categoria))
+            throw new Exception("O produto deve ter uma categoria");
+        if(quantidadeEmEstoque <= 0)
+            throw new Exception("O produto deve ter um estoque de no mínimo 1");
+
+        Descricao = descricao;
+        Valor = valor;
+        Imagem = imagem;
+        Categoria = categoria;
+        QuantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
---
+++ src/Tests/BackCommerce.Tests/Unit/Produtos/ProdutoUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using BackCommerce.Domain.Produtos;
+using Xunit;
+
+namespace BackCommerce.Tests.Unit.Produtos;
+
+public class ProdutoUnitTests
+{
+    [Fact]
+    public void Deve_Cadastrar_Produto()
+    {
+        // Arrange
+        string descricao = "Fone de ouvido";
+        decimal valor = 100;
+        string imagem = "Imagem";
+        string categoria = "Eletronico";
+        int quantidadeEmEstoque = 1;
+
+        // Act
+        var produto = new Produto(descricao, valor, imagem, categoria, quantidadeEmEstoque);
+
+        // Assert
+        Assert.NotNull(produto);
+    }
+}
